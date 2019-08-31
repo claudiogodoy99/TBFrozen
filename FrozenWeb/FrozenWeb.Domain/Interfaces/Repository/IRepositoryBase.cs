f2ed9905857dfa3739a92d872703bc65f1985b4a
claudio godoy <popo_dvs2009@hotmail.com>
2019-08-31 17:24:57 -0300

    Criação da classe unit of Work v1.0

--- FrozenWeb/FrozenWeb.Domain/Interfaces/Repository/IRepositoryBase.cs
+++ FrozenWeb/FrozenWeb.Domain/Interfaces/Repository/IRepositoryBase.cs
@@ -12,29 +12,53 @@ namespace FrozenWeb.Domain.Interfaces.Repository
          IQueryable<TEntity> QueryAble { get;}
 
         void Add(TEntity entity);
+
         Task AddAsync(TEntity entity);
+
         bool Any(Expression<Func<TEntity, bool>> where);
+
         Task<bool> AnyAsync(Expression<Func<TEntity, bool>> where);
+
         int Count(Expression<Func<TEntity, bool>> where);
+
         Task<int> CountAsync(Expression<Func<TEntity, bool>> where);
+
         void Delete(params object[] key);
+
         IEnumerable<TEntityResult> Distnct<TEntityResult>(Expression<Func<TEntity,TEntityResult>> select);
+
         IEnumerable<TEntityResult> Distnct<TEntityResult>(Expression<Func<TEntity, bool>> where, Expression<Func<TEntity, TEntityResult>> select);
+
         Task<IEnumerable<TEntityResult>> DistnctAsync<TEntityResult>(Expression<Func<TEntity, bool>> where, Expression<Func<TEntity, TEntityResult>> select);
+
         Task<IEnumerable<TEntityResult>> DistnctAsync<TEntityResult>(Expression<Func<TEntity, TEntityResult>> select);
+
         TEntity FirstOrDefault(params Expression<Func<TEntity, object>>[] include);
+
         TEntity FirstOrDefault(Expression<Func<TEntity, bool>> where,params Expression<Func<TEntity, object>>[] include);
+
         Task<TEntity> FirstOrDefaultAsync(params Expression<Func<TEntity, object>>[] include);
+
         Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] include);
+
         IEnumerable<TEntity> List(params Expression<Func<TEntity, object>>[] include);
+
         IEnumerable<TEntity> List(Expression<Func<TEntity, bool>> where,params Expression<Func<TEntity, object>>[] include);
+
         Task<IEnumerable<TEntity>> ListAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] include);
+
         Task<IEnumerable<TEntity>> ListAsync(params Expression<Func<TEntity, object>>[] include);
+
         TEntity Select(string id);
+
         Task<TEntity> SelectAsync(string id);
+
         TEntity Select(int id);
+
         Task<TEntity> SelectAsync(int id);
+
         void Update(TEntity entity, params object[] keys);
+
         Task UpdateAsync(TEntity entity, params object[] keys);
     }
 }
---
+++ FrozenWeb/FrozenWeb.Infra.Data/UoW/UnityofWork.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Threading.Tasks;
+using FrozenWeb.Infra.Data.Context;
+using FrozenWeb.Infra.Data.Interfaces;
+
+namespace FrozenWeb.Infra.Data.UoW
+{
+    public class UnityofWork : IUnityofWork
+    {
+
+        private readonly FrozenContext _context;
+        private bool _dispose;
+
+        public UnityofWork(FrozenContext context)
+        {
+            _context = context;
+        }
+
+        #region Metodos_Publicos
+
+        public DbContextTransaction BeginTransaction() => _context.Database.BeginTransaction();
+
+        public void Commit(DbContextTransaction transaction) => transaction.Commit();
+
+        public void RollBack(DbContextTransaction transaction) => transaction.Rollback();
+
+        public void SaveChanges() => _context.SaveChanges();
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+
+        #endregion
+
+
+        #region Metodos_Protegidos
+
+
+        protected virtual void ThrowSaveChangesError(DbEntityValidationException e)
+        {
+            var errorMessages = e.EntityValidationErrors
+                .SelectMany(ve => ve.ValidationErrors)
+                .Select(msg => msg.ErrorMessage);
+
+            var allErrorMessages = string.Join(";", errorMessages);
+            var exceptionMsg = $"Erro do ao salvar mudanças-> {e.Message}; \n Todos os erros => {allErrorMessages} ";
+
+            throw new DbEntityValidationException(exceptionMsg, e.EntityValidationErrors);
+
+            //Adicionar #Log# Posteriormente
+        }
+
+
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_dispose)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _dispose = true;
+
+        }
+
+
+        #endregion
+
+    }
+}
