c4ab1f7d548aff9e7fffce70cf5cc695b25ab9d6
1GabrielMachado1 <gabriel.ferreira.machado@hotmail.com>
2019-09-08 13:49:36 -0300

    Criação da solução FrozenWebClientSide e configuração da estrutura básica das pastas dos aquivos.
    Instalação do gulp e criação do gulp file.

---
+++ FrozenWebClientSide/FrozenWebClientSide/gulpfile.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('source-files')
+        .pipe(sass())
+        .pipe(gulp.dest('destination'))
+});
