420f42f77533c63b9715492bd5a812fd7e12607c
1GabrielMachado1 <gabriel.ferreira.machado@hotmail.com>
2019-09-19 23:58:58 -0300

    Desenvolvimento das páginas home e CRUD do cliente. Alterações na página index.

---
+++ FrozenWebClientSide/FrozenWebClientSide/RawAssets/ts/user-data-table.ts
@@ -0,0 +1,32 @@
+﻿(($: JQueryStatic) => {
+
+  $(() => {
+
+    //#region Configurações Globais
+
+    /* Ativando as tooltips dos botões de ação */
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* Definindo comportamento das checkboxes das linhas da tabela */
+    let checkbox = $('table tbody input[type="checkbox"]');
+    $("#selectAll").click(function () {
+      if (this['checked']) {
+        checkbox.each(function () {
+          this['checked'] = true;
+        });
+      } else {
+        checkbox.each(function () {
+          this['checked'] = false;
+        });
+      }
+    });
+    checkbox.click(function () {
+      if (!this['checked']) {
+        $("#selectAll").prop("checked", false);
+      }
+    });
+
+    //#endregion
+
+  })
+})(jQuery);
