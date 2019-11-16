bd709d85f7e8fd93f1d9358233e8a1a78f870bee
claudio godoy <popo_dvs2009@hotmail.com>
2019-11-15 22:13:45 -0300

    Controller Finalizado

--- FrozenWeb/FrozenWeb.Presentation.Api/Controllers/EmpresaController.cs
+++ FrozenWeb/FrozenWeb.Presentation.Api/Controllers/EmpresaController.cs
@@ -31,5 +31,47 @@ namespace FrozenWeb.Presentation.Api.Controllers
                 return InternalServerError();
             }
         }
+
+        [HttpPut]
+        public IHttpActionResult Atualizar(EmpresaViewModel empresa)
+        {
+            try
+            {
+                _empresa.Update(empresa, empresa.cnpj);
+                return Ok();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/Empresa/Deletar/{cnpj}")]
+        public IHttpActionResult Deletar([FromUri]string cnpj)
+        {
+            try
+            {
+                _empresa.Delete(cnpj);
+                return Ok();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+
+        [HttpGet]
+        public JsonResult<List<EmpresaViewModel>> ListarTodas()
+        {
+            try
+            {
+                return Json(_empresa.List().ToList());
+            }
+            catch
+            {
+                return Json(new List<EmpresaViewModel>());
+            }
+        }
     }
 }
