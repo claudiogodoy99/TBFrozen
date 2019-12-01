7c3984d4f0a18c7277e957a1bcc823cb9407d3f4
claudio godoy <popo_dvs2009@hotmail.com>
2019-12-01 11:20:37 -0300

    BUG

--- FrozenWeb/FrozenWeb.Infra.Data/Repository/VeiculoRepository.cs
+++ FrozenWeb/FrozenWeb.Infra.Data/Repository/VeiculoRepository.cs
@@ -1,11 +1,12 @@
 ﻿
 
 using FrozenWeb.Domain.Enities;
+using FrozenWeb.Domain.Interfaces.Repository;
 using FrozenWeb.Infra.Data.Context;
 
 namespace FrozenWeb.Infra.Data.Repository
 {
-    public class VeiculoRepository : RepostitoryBase<Veiculo>
+    public class VeiculoRepository : RepostitoryBase<Veiculo>, IVeiculoRepository
     {
         public VeiculoRepository(FrozenContext context) : base(context)
         {
