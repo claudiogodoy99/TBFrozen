af108300be5da5dc3794b4243a14f4407d91168a
claudio godoy <popo_dvs2009@hotmail.com>
2019-12-02 23:24:06 -0300

    Entidade de Viagem

---
+++ FrozenWeb/FrozenWeb.Domain/Enities/Viagem.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace FrozenWeb.Domain.Enities
+{
+    public class Viagem : EntityBase
+    {
+        public int? id { get; set; }
+        public DateTime dataDaSaida { get; set; }
+        public int garagemId { get; set; }
+        public string cnpjId { get; set; }
+        public int veiculoId { get; set; }
+        public string cnhMotorista { get; set; }
+        public DateTime PrevisaoDeVolta { get; set; }
+        public DateTime? VoltaReal { get; set; }
+        public string RelatorioDeViagemFinalizada { get; set; }
+
+        public virtual Empresa empresa { get; set; }
+        public virtual Veiculo veiculo { get; set; }
+        public virtual Motorista motorista { get; set; }
+        public virtual Garagem garagemEntrada { get; set; }
+
+        public bool ViagemEstaEmAndamento
+        {
+            get => !VoltaReal.HasValue;
+        }
+
+        public bool ViagemAtrasada
+        {
+            get => PrevisaoDeVolta.Date > DateTime.Now && ViagemEstaEmAndamento;
+        }
+
+        public bool ViagemQuaseConcluida {
+            get => PrevisaoDeVolta.Date >= PrevisaoDeVolta.Date.AddHours(-2) && !ViagemAtrasada && ViagemEstaEmAndamento;
+        }
+
+        public bool ViagemConcluida {
+            get => !VoltaReal.HasValue;
+        }
+            
+    }
+}
---
+++ FrozenWeb/FrozenWeb.Domain/Interfaces/Repository/IViagemRepository.cs
@@ -0,0 +1,9 @@
+﻿
+using FrozenWeb.Domain.Enities;
+
+namespace FrozenWeb.Domain.Interfaces.Repository
+{
+    public interface IViagemRepository : IRepositoryBase<Veiculo>
+    {
+    }
+}
